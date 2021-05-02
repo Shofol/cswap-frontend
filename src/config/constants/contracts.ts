8ea73b2798be71f4194ed84af415862fd4aa6246
John Doe <johndoe@example.com>
2021-05-01 21:20:45 -0700

    Farm POC

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,15 +1,18 @@
 export default {
   cake: {
-    56: '0xfaD0ac6bfF4ad39d28d4aa2eCDEe954D168bD418',
+    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
     97: '',
+    137: '0x62801B19b0ea8f094AD17f7DE824EEb4C54C6530', // KRILL
   },
   masterChef: {
-    56: '0xba806cF64D1F9eeB981b79405eeb42D5733561b5',
+    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
     97: '',
+    137: '0x5F927189831EeAc858daEAe5a35e3584f76877E5' 
   },
   wbnb: {
     56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
     97: '',
+    137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270' // WMATIC
   },
   lottery: {
     56: '',
@@ -22,9 +25,11 @@ export default {
   mulltiCall: {
     56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
     97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+    137: '0x95028E5B8a734bb7E2071F96De89BABe75be9C8E',
   },
   busd: {
     56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
     97: '',
+    137: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174' // USDC
   },
 }
